2511d08ceb878a35c553859a750a79e56607ca30
Sam <sampenland@gmail.com>
2019-07-24 12:57:28 -0400

    Project Start

---
+++ SharkEdit/FrmMain.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace SharkEdit
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDisplay = new System.Windows.Forms.DataGridView();
+            this.btnLoadFile = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDisplay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDisplay
+            // 
+            this.dgvDisplay.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDisplay.Location = new System.Drawing.Point(12, 41);
+            this.dgvDisplay.Name = "dgvDisplay";
+            this.dgvDisplay.Size = new System.Drawing.Size(489, 397);
+            this.dgvDisplay.TabIndex = 0;
+            // 
+            // btnLoadFile
+            // 
+            this.btnLoadFile.Location = new System.Drawing.Point(12, 12);
+            this.btnLoadFile.Name = "btnLoadFile";
+            this.btnLoadFile.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadFile.TabIndex = 1;
+            this.btnLoadFile.Text = "Load File";
+            this.btnLoadFile.UseVisualStyleBackColor = true;
+            this.btnLoadFile.Click += new System.EventHandler(this.btnLoadFile_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 450);
+            this.Controls.Add(this.btnLoadFile);
+            this.Controls.Add(this.dgvDisplay);
+            this.Name = "FrmMain";
+            this.Text = "SharkEdit";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDisplay)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDisplay;
+        private System.Windows.Forms.Button btnLoadFile;
+    }
+}
+
---
+++ SharkEdit/FrmMain.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SharkEdit
+{
+    public partial class FrmMain : Form
+    {
+        // Buffer for Loaded File (pre-changes)
+        public byte[] CurrentFileContents;
+
+        // Buffer for Loaded File (post-changes)
+        public byte[] FileContents;
+
+        // Keep track of first loaded file
+        private bool FirstLoad = true;
+
+        // Form start
+        public FrmMain()
+        {
+            // Load components
+            InitializeComponent();
+        }
+
+        // Click of load file button
+        private void btnLoadFile_Click(object sender, EventArgs e)
+        {
+            // Create and show dialog to get filepath for 
+            // desired file
+            OpenFileDialog file_dialog = new OpenFileDialog();
+            file_dialog.Filter = "Cap Files|*.cap";
+            file_dialog.ShowDialog();
+
+            // Load file then process
+            LoadFile(file_dialog.FileName, FirstLoad ? false : true);
+        }
+
+        private void LoadFile(string filename, bool confirm_message)
+        {
+            // Display message to make sure user wants to clear
+            // all old data and re-load file
+            if (confirm_message)
+            {
+                if (MessageBox.Show("Are you sure you wish to clear all unsaved changes?", "Re-load New File?", MessageBoxButtons.YesNo) == DialogResult.No) return;
+            }
+
+            // No longer first load
+            FirstLoad = false;
+
+            // Clear all old values
+            dgvDisplay.Rows.Clear();
+
+            // Clear buffers (precaution, they should be 
+            // cleared when loaded anyway
+            CurrentFileContents = new byte[1];
+            FileContents = new byte[1];
+
+            // Erase old file and load new file
+            CurrentFileContents = File.ReadAllBytes(filename);
+            FileContents = File.ReadAllBytes(filename);
+
+            ProcessFile(filename);
+
+        }
+
+        private void ProcessFile(string filename)
+        {
+
+            // Break file into packets
+            List<byte[]> Packets = new List<byte[]>();
+
+            // Loop through all packets
+
+            // Header Total Lenght = 15 bytes
+
+            // Header[0]-[3] => Time Stamps
+
+            // Header[4]-[7] => Time Intervals
+
+            // Header[8]-[11] => Length of Packet
+
+            // Header[12]-[15] => Lenght of Packet (again)
+
+        }
+    }
+}
